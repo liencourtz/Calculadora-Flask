29e3740879d5ea39aa816c873e2038bc3d214d27
filipessilva <filipe.ssilva97@gmail.com>
2022-08-29 21:30:27 -0300

    up

--- calculadora.py
+++ calculadora.py
@@ -5,13 +5,13 @@ class calculadora:
         pass
 
     def somar(self, num1, num2):
-        return num1 + num2
+        return float(num1) + float(num2)
 
     def subtrair(self, num1, num2):
-        return num1 - num2
+        return float(num1) - float(num2)
 
     def multiplicar(self, num1, num2):
-        return num1 * num2
+        return float(num1) * float(num2)
 
     def dividir(self, num1, num2):
         if num2 == 0:
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from flask import request, redirect
 from calculadora import calculadora
+import this
 
 app = Flask(__name__)  # Referência ao objeto FLASK, criando uma variável APP que guarda os elementos do FLASK
 calc = calculadora()
+this.resultadoFinal= ""
 
 @app.route("/")  # Página Index = Primeira Página de Qualquer Site
 def homepage():
@@ -15,8 +17,8 @@ def soma():
     if request.method == 'POST':
         numero1 = request.form['num1']
         numero2 = request.form['num2']
-        resultadoFinal = calc.somar(numero1,numero2)
-        return render_template("soma.html", titulo="Soma", resultado=resultadoFinal)
+        this.resultadoFinal = calc.somar(numero1,numero2)
+    return render_template("soma.html", titulo="Soma", resultado=this.resultadoFinal)
 
 if __name__ == '__main__':
     app.run(debug=True)
